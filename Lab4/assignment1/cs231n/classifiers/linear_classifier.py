dd43f30e6cdc82e1a64a088b8430c41b1f181af2
abdo <abdoyasser702@gmail.com>
2018-12-14 01:28:23 +0200

    Implement a Softmax classifier finished

--- Lab4/assignment1/cs231n/classifiers/linear_classifier.py
+++ Lab4/assignment1/cs231n/classifiers/linear_classifier.py
@@ -37,40 +37,44 @@ class LinearClassifier(object):
     # Run stochastic gradient descent to optimize W
     loss_history = []
     for it in range(num_iters):
-      X_batch = None
-      y_batch = None
 
-      #########################################################################
-      # TODO:                                                                 #
-      # Sample batch_size elements from the training data and their           #
-      # corresponding labels to use in this round of gradient descent.        #
-      # Store the data in X_batch and their corresponding labels in           #
-      # y_batch; after sampling X_batch should have shape (dim, batch_size)   #
-      # and y_batch should have shape (batch_size,)                           #
-      #                                                                       #
-      # Hint: Use np.random.choice to generate indices. Sampling with         #
-      # replacement is faster than sampling without replacement.              #
-      #########################################################################
-      pass
-      #########################################################################
-      #                       END OF YOUR CODE                                #
-      #########################################################################
+        X_batch = None
+        y_batch = None
 
-      # evaluate loss and gradient
-      loss, grad = self.loss(X_batch, y_batch, reg)
-      loss_history.append(loss)
+        #########################################################################
+        # TODO:                                                                 #
+        # Sample batch_size elements from the training data and their           #
+        # corresponding labels to use in this round of gradient descent.        #
+        # Store the data in X_batch and their corresponding labels in           #
+        # y_batch; after sampling X_batch should have shape (dim, batch_size)   #
+        # and y_batch should have shape (batch_size,)                           #
+        #                                                                       #
+        # Hint: Use np.random.choice to generate indices. Sampling with         #
+        # replacement is faster than sampling without replacement.              #
+        #########################################################################
+        batch_ind = np.random.choice(num_train, batch_size)
+        X_batch = X[batch_ind]
+        y_batch = y[batch_ind]
 
-      # perform parameter update
-      #########################################################################
-      # TODO:                                                                 #
-      # Update the weights using the gradient and the learning rate.          #
-      #########################################################################
-      pass
-      #########################################################################
-      #                       END OF YOUR CODE                                #
-      #########################################################################
+        #########################################################################
+        #                       END OF YOUR CODE                                #
+        #########################################################################
 
-      if verbose and it % 100 == 0:
+        # evaluate loss and gradient
+        loss, grad = self.loss(X_batch, y_batch, reg)
+        loss_history.append(loss)
+
+        # perform parameter update
+        #########################################################################
+        # TODO:                                                                 #
+        # Update the weights using the gradient and the learning rate.          #
+        #########################################################################
+        self.W += - learning_rate * grad  
+        #########################################################################
+        #                       END OF YOUR CODE                                #
+        #########################################################################
+
+    if verbose and it % 100 == 0:
         print('iteration %d / %d: loss %f' % (it, num_iters, loss))
 
     return loss_history
@@ -94,7 +98,8 @@ class LinearClassifier(object):
     # TODO:                                                                   #
     # Implement this method. Store the predicted labels in y_pred.            #
     ###########################################################################
-    pass
+    scores = X.dot(self.W)
+    y_pred = np.argmax(scores, axis=1)
     ###########################################################################
     #                           END OF YOUR CODE                              #
     ###########################################################################
--- Lab4/assignment1/cs231n/classifiers/softmax.py
+++ Lab4/assignment1/cs231n/classifiers/softmax.py
@@ -1,62 +1,65 @@
 import numpy as np
 from random import shuffle
 
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    return np.exp(x) / np.sum(np.exp(x) , axis = -1 , keepdims = True)
+
+def get_one_hot(targets, nb_classes):
+        res = np.eye(nb_classes)[np.array(targets).reshape(-1)]
+        return res.reshape(list(targets.shape)+[nb_classes]) 
+
 def softmax_loss_naive(W, X, y, reg):
-  """
-  Softmax loss function, naive implementation (with loops)
+    """
+    Softmax loss function, naive implementation (with loops)
 
-  Inputs have dimension D, there are C classes, and we operate on minibatches
-  of N examples.
+    Inputs have dimension D, there are C classes, and we operate on minibatches
+    of N examples.
 
-  Inputs:
-  - W: A numpy array of shape (D, C) containing weights.
-  - X: A numpy array of shape (N, D) containing a minibatch of data.
-  - y: A numpy array of shape (N,) containing training labels; y[i] = c means
+    Inputs:
+    - W: A numpy array of shape (D, C) containing weights.
+    - X: A numpy array of shape (N, D) containing a minibatch of data.
+    - y: A numpy array of shape (N,) containing training labels; y[i] = c means
     that X[i] has label c, where 0 <= c < C.
-  - reg: (float) regularization strength
+    - reg: (float) regularization strength
 
-  Returns a tuple of:
-  - loss as single float
-  - gradient with respect to weights W; an array of same shape as W
-  """
-  # Initialize the loss and gradient to zero.
-  loss = 0.0
-  dW = np.zeros_like(W)
+    Returns a tuple of:
+    - loss as single float
+    - gradient with respect to weights W; an array of same shape as W
+    """
+    
+    return softmax_loss_vectorized(W, X, y, reg)
 
-  #############################################################################
-  # TODO: Compute the softmax loss and its gradient using explicit loops.     #
-  # Store the loss in loss and the gradient in dW. If you are not careful     #
-  # here, it is easy to run into numeric instability. Don't forget the        #
-  # regularization!                                                           #
-  #############################################################################
-  pass
-  #############################################################################
-  #                          END OF YOUR CODE                                 #
-  #############################################################################
-
-  return loss, dW
 
 
 def softmax_loss_vectorized(W, X, y, reg):
-  """
-  Softmax loss function, vectorized version.
+    """
+    Softmax loss function, vectorized version.
 
-  Inputs and outputs are the same as softmax_loss_naive.
-  """
-  # Initialize the loss and gradient to zero.
-  loss = 0.0
-  dW = np.zeros_like(W)
-
-  #############################################################################
-  # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
-  # Store the loss in loss and the gradient in dW. If you are not careful     #
-  # here, it is easy to run into numeric instability. Don't forget the        #
-  # regularization!                                                           #
-  #############################################################################
-  pass
-  #############################################################################
-  #                          END OF YOUR CODE                                 #
-  #############################################################################
-
-  return loss, dW
+    Inputs and outputs are the same as softmax_loss_naive.
+    """
+    # Initialize the loss and gradient to zero.
+    loss = 0.0
+    dW = np.zeros_like(W)
+    N, D = X.shape
+    num_classes = W.shape[1]
+    #############################################################################
+    # TODO: Compute the softmax loss and its gradient using explicit loops.     #
+    # Store the loss in loss and the gradient in dW. If you are not careful     #
+    # here, it is easy to run into numeric instability. Don't forget the        #
+    # regularization!                                                           #
+    #############################################################################
+    scores = X.dot(W)
+    scores = softmax(scores)
+    # regularization loss
+    reg_loss = reg * np.sum(np.multiply(W, W))
+    # cross-entropy-loss
+    one_hot = get_one_hot(y, num_classes)
+    loss =  - 1/N * np.sum( one_hot * np.log(scores) )
+    loss += reg_loss
+    # gradient
+    dldz2 = scores - one_hot 
+    dz2dw2 = X
+    dW = 1/N * np.dot(dz2dw2.T, dldz2) + 2 * reg * W    
+    return loss, dW
 
